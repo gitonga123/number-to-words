6403ba3cd03356ba20acd8fdf9adef497dc75717
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-10-26 13:32:00 +0300

    Swahili Dictrionary

--- src/Language/Swahili/SwahiliTripletTransformer.php
+++ src/Language/Swahili/SwahiliTripletTransformer.php
@@ -1,7 +1,7 @@
 <?php
 namespace NumberToWords\Language\Swahili;
 use NumberToWords\Language\TripletTransformer;
-class SwahiliTripletTransformer extends TripletTransformer
+class SwahiliTripletTransformer extends TripletTransformer;
 {
     private $dictionary;
 
@@ -12,7 +12,32 @@ class SwahiliTripletTransformer extends TripletTransformer
 
     public function transformToWords($number) {
         $units = $number % 10;
-        $tens = 
+        $tens = (int) ($number / 10) % 10;
+        $hundrends = (int) ($number / 100) % 10;
+
+        if ($hundrends > 0) {
+            $words[] = $this->dictionary->getCorrespondingHundred($hundrends);
+        }
+
+        if ($tens !== 0 || $units !== 0) {
+            $words[] = $this->getSubHundred($tens, $units);
+        }
+
     }
 
+    private function getSubHundred($tens, $units)
+    {
+        $words = [];
+        if ($tens === 1) {
+            $words[] = $this->dictionary->getCorrespondingTeen($units);
+        } else {
+            if ($tens > 0) {
+                $words[] = $this->dictionary->getCorrespondingTen($tens);
+            }
+            if ($units > 0) {
+                $words[] = $this->dictionary->getCorrespondingUnit($units);
+            }
+        }
+        return implode('-', $words);
+    }
 }
